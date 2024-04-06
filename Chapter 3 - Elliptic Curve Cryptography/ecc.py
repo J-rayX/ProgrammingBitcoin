26fbe450318000497081518db71b28683f42928f
jkaylight <jkaylight@gmail.com>
2024-04-06 05:47:18 +0100

    Ch3: Elliptical Curve Cryptography. Ex3: Test for additions

---
+++ Chapter 3 - Elliptic Curve Cryptography/ecc.py
@@ -0,0 +1,171 @@
+import unittest
+from unittest import TestCase
+
+
+class FieldElement:
+    def __init__(self, num, prime):
+        if num >= prime or num < 0:
+            error = "Num {} not in field range 0 to {}".format(num, prime - 1)
+            raise ValueError(error)
+        self.num = num
+        self.prime = prime
+
+    def __repr__(self):
+        return "FieldElement_{}({})".format(self.num, self.prime)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other):
+        if other is None:
+            return False
+        return self.num != other.num and self.prime != other.prime
+
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Cannot add two numbers in different fields")
+        num = (self.num + other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Cannot add two numbers in different fields")
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __mul__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Cannot add two numbers in different fields")
+        num = (self.num * other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __pow__(self, exponent):
+        n = exponent % (self.prime - 1)
+        # num = (self.num**exponent) % self.prime
+        num = pow(self.num, n, self.prime)
+        return self.__class__(num, self.prime)
+
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Cannot divide two numbers in different Fields")
+        # num = (self.num / other.num) % self.prime
+        num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
+        return self.__class__(num, self.prime)
+
+
+class Point:
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        # don't check if curve equation is satisfied when point is at infinity
+        if self.x is None and self.y is None:
+            return
+        if self.y**2 != self.x**3 + a * x + b:
+            raise ValueError("({}, {}) is not on the curve".format(x, y))
+        # if two points have same x but different y i.e vertical line, additive inverses
+        # additive inverses: a number when added (or sign changed) to original number results in zero
+        if self.x == x and self.y != y:
+            return self.__class__(None, None, self.a, self.b)
+
+    def __eq__(self, other):
+        return (
+            self.x == other.x
+            and self.y == other.y
+            and self.a == other.a
+            and self.b == other.b
+        )
+
+    def __ne__(self, other):
+        return (
+            self.x != other.x
+            and self.y != other.y
+            and self.a != other.a
+            and self.b != other.b
+        )
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError(
+                "Points {}, {} are not on the same curve".format(self, other)
+            )
+        # point at infinity
+        if self.x is None:
+            return other
+        # point at infinity
+        if other.x is None:
+            return self
+
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x3 = s**2 - self.x - other.x
+            y3 = s * (self.x - x3) - self.y
+            return self.__class__(x3, y3, self.a, self.b)
+
+        if self == other:
+            s = (3 * self.x**2 + self.a) / (2 * self.y)
+            x3 = s**2 - 2 * self.x
+            y3 = s * (self.x - x3) - self.y
+            return self.__class__(x3, y3, self.a, self.b)
+
+        # Exception: when tangent line is vertical
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+
+    def __rmul__(self, coefficient):
+        coef = coefficient
+        current = self
+        result = self.__class__(None, None, self.a, self.b)
+        while coef:
+            if coef & 1:
+                result += current
+            current += current
+            coef >>= 1
+        return result
+
+        # product = self.__class__(None, None, self.a, self.b)
+        # for _ in range(coefficient):
+        #     product += self
+        # return product
+
+
+class ECCTest(TestCase):
+    def test_on_curve(self):
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+        valid_points = ((192, 105), (17, 56), (1, 193))
+        invalid_points = ((200, 119), (42, 99))
+        for x_raw, y_raw in valid_points:
+            x = FieldElement(x_raw, prime)
+            y = FieldElement(y_raw, prime)
+        Point(x, y, a, b)
+        for x_raw, y_raw in invalid_points:
+            x = FieldElement(x_raw, prime)
+            y = FieldElement(y_raw, prime)
+            with self.assertRaises(ValueError):
+                Point(x, y, a, b)
+
+    def test_add(self):
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(0, prime)
+        additions = (
+            (192, 105, 17, 56, 170, 142),
+            (47, 71, 117, 141, 60, 139),
+            (143, 98, 76, 66, 47, 71),
+        )
+        for x1_raw, y1_raw, x2_raw, y2_raw, x3_raw, y3_raw in additions:
+            x1 = FieldElement(x1_raw, prime)
+            y1 = FieldElement(y1_raw, prime)
+            p1 = Point(x1, y1, a, b)
+            x2 = FieldElement(x2_raw, prime)
+            y2 = FieldElement(y2_raw, prime)
+            p2 = Point(x2, y2, a, b)
+            x3 = FieldElement(x3_raw, prime)
+            y3 = FieldElement(y3_raw, prime)
+            p3 = Point(x3, y3, a, b)
+            self.assertEqual(p1 + p2, p3)
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Reading
 
-Examples and exercises from:
+My solutions to the excercises from:
 
 Programming Bitcoin: Learn How to Program Bitcoin from Scratch
 
